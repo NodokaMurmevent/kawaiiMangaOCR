fa183edbba9fe3e4950eadb78d3084b6423a9548
NodokaMurmevent <nodoka@symphonie-of-code.fr>
2023-06-18 16:23:40 +0200

    stracking

--- src/Controller/StableController.php
+++ src/Controller/StableController.php
@@ -5,12 +5,23 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use MatomoTracker;
 
 class StableController extends AbstractController
 {
     #[Route('/stable-diffusion', name: 'app_stable')]
     public function index(): Response
     {
+
+        $matomoSiteId = 61;
+        $matomoUrl = "https://piwik.imago-design.net"; 
+        $matomoToken = "adf36114f2965ae516ac5cdd9e3a24e1";        
+  
+        $matomoPageTitle = "Another beginner guide to generate your local Waifu | Ni~ Coni CoNiiiiiifusion";
+        $matomoTracker = new MatomoTracker((int)$matomoSiteId, $matomoUrl);
+        $matomoTracker->setTokenAuth($matomoToken);
+        $matomoTracker->doTrackPageView($matomoPageTitle);
+
         return $this->render('stable/index.html.twig', [
             'controller_name' => 'StableController',
         ]);
