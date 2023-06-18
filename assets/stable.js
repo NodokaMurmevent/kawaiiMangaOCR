0b3a23176bf739db160ce57c75642006d562607f
NodokaMurmevent <nodoka@symphonie-of-code.fr>
2023-06-18 09:28:51 +0200

    stable diffusion 2

--- assets/stable.js
+++ assets/stable.js
@@ -14,8 +14,22 @@
 
 import './styles/stable.css';
 import Iconify from '@iconify/iconify';
+import * as tocbot from 'tocbot';
+
 //import hljs from 'highlight.js';
 
-//document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", () => {
 //   hljs.highlightAll();
-//});
+    tocbot.init({
+        // Where to render the table of contents.
+        tocSelector: '.js-toc',
+        // Where to grab the headings to build the table of contents.
+        contentSelector: '.js-toc-content',
+        ignoreSelector: '.js-toc-ignore',
+        // Which headings to grab inside of the contentSelector element.
+        headingSelector: 'h2, h3, h4',
+        // For headings inside relative or absolute positioned containers within content.
+        hasInnerContainers: true,
+    });
+    
+});
